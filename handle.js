87e55e6a8973542659b0da9f8b88a77e4b06f88a
eboboeing-747 <147101544+eboboeing-747@users.noreply.github.com>
2024-10-24 04:53:06 +0300

    service order form added
    added: handle.js, form.html, form.css
    todo: link handle.js open-form script properly

---
+++ handle.js
@@ -0,0 +1,9 @@
+document.getElementById("open-form").addEventListener("click", function()
+{
+    document.getElementById("form-modal").classList.add("open");
+})
+
+document.getElementById("close-form").addEventListener("click", function()
+{
+    document.getElementById("form-modal").classList.remove("open");
+})
