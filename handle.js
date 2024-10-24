19fabd61eb60ceb841eb487fc289fe70f50c0cc1
eboboeing-747 <147101544+eboboeing-747@users.noreply.github.com>
2024-10-24 15:30:14 +0300

    handle.js improved
    form now closes if esc is pressed or mouse is clicked outside of it

--- handle.js
+++ handle.js
@@ -7,3 +7,25 @@ document.getElementById("close-form").addEventListener("click", function()
 {
     document.getElementById("form-modal").classList.remove("open");
 })
+
+window.addEventListener('keydown', (e) =>
+{
+    if (e.key === "Escape")
+    {
+        document.getElementById("form-modal").classList.remove("open")
+    }
+})
+
+document.querySelector("#form-modal .modal-content").addEventListener("click", event =>
+{
+    event._isClickWithInModal = true;
+})
+
+document.getElementById("form-modal").addEventListener("click", event => 
+{
+    if (event._isClickWithInModal)
+    {
+        return;
+    }
+    event.currentTarget.classList.remove("open")
+})
