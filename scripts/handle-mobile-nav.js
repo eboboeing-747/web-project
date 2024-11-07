7952e57f8ee67b60c9a47a8036bb332a3f3e81fa
eboboeing-747 <147101544+eboboeing-747@users.noreply.github.com>
2024-11-07 03:37:08 +0300

    adaptive layout added
    file structure improved
    added mobile navigation bar button and scripts

---
+++ scripts/handle-mobile-nav.js
@@ -0,0 +1,33 @@
+// mobile nav open script
+document.getElementById("open-mobile-nav").addEventListener("click", function()
+{
+    document.getElementById("mobile-nav-modal").classList.add("open");
+})
+
+// mobile nav close scripts
+document.getElementById("close-mobile-nav").addEventListener("click", function()
+{
+    document.getElementById("mobile-nav-modal").classList.remove("open");
+})
+
+window.addEventListener('keydown', (e) =>
+{
+    if (e.key === "Escape")
+    {
+        document.getElementById("mobile-nav-modal").classList.remove("open")
+    }
+})
+
+document.querySelector("#mobile-nav-modal .modal-content").addEventListener("click", event =>
+{
+    event._isClickWithInModal = true;
+})
+
+document.getElementById("mobile-nav-modal").addEventListener("click", event => 
+{
+    if (event._isClickWithInModal)
+    {
+        return;
+    }
+    event.currentTarget.classList.remove("open")
+})
