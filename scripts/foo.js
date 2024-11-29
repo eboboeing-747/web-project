6d2a218729cff5ee40a49732d1fd0cd7e5a5769c
eboboeing-747 <147101544+eboboeing-747@users.noreply.github.com>
2024-11-29 19:19:05 +0300

    auth form returned

---
+++ scripts/foo.js
@@ -0,0 +1,9 @@
+// let asdfa = document.getElementsByTagName("h4");
+// console.log(typeof(asdfa));
+// asdfa.textContent = "welcome to out site";
+// console.log(asdfa);
+
+// 1. find <h4> and replace its text with 'welcome to our site'
+// 2. change its color to red
+// 3. find <p> and replace its text with 'this is new paragrahp text'
+// 4. make video.display = none
